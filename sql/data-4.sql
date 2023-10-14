dc6c320af58b2207216f1179f53287ddee17cdc9
dirkarnez <smalldirkalex@gmail.com>
2023-10-14 22:00:29 +0800

    - wip

--- sql/data-4.sql
+++ sql/data-4.sql
@@ -1,5 +1,5 @@
-INSERT INTO curriculum_course_you_tube_videos (`url`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/0SLnKsFWwFA', `id`
+INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
+SELECT 'https://www.youtube.com/embed/0SLnKsFWwFA', '', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Elementary';
 
