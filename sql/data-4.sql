a0b267297845fa9f301702d935c60259f5c8cfb6
dirkarnez <smalldirkalex@gmail.com>
2023-10-16 18:34:10 +0800

    - update sql/data-4.sql

--- sql/data-4.sql
+++ sql/data-4.sql
@@ -1,128 +1,128 @@
 -- Coding Minecraft
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/0SLnKsFWwFA', '', `id`
+SELECT 'https://www.youtube.com/embed/0SLnKsFWwFA', '2019 STEMex Summer Camp - Technology & Coding - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Elementary';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/vMkAZw6nFK4', '', `id`
+SELECT 'https://www.youtube.com/embed/vMkAZw6nFK4', 'STEMex Coding Minecraft - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Introductory';
 
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', '', `id`
+SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', 'Minecraft STEM - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Intermediate';
 
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', '', `id`
+SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', 'Minecraft STEM - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Advanced';
 
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', '', `id`
+SELECT 'https://www.youtube.com/embed/DofLMIvBQ5k', 'Minecraft STEM - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Minecraft Master';
 
 -- Coding Roblox
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/CW0RrMdB0kQ', '', `id`
+SELECT 'https://www.youtube.com/embed/CW0RrMdB0kQ', 'STEMex Coding Roblox - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Roblox Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/CW0RrMdB0kQ', '', `id`
+SELECT 'https://www.youtube.com/embed/CW0RrMdB0kQ', 'STEMex Coding Roblox - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Roblox Intermediate';
 
 -- Coding Python
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', '', `id`
+SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', 'Senior Python Programming Course promotion video - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Python Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', '', `id`
+SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', 'Senior Python Programming Course promotion video - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Python Intermediate';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', '', `id`
+SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', 'Senior Python Programming Course promotion video - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Python Advanced';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', '', `id`
+SELECT 'https://www.youtube.com/embed/hh3W_tjPGlI', 'Senior Python Programming Course promotion video - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Python Master';
 
 -- Coding Scratch
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/8qjpKUmJ9zk', '', `id`
+SELECT 'https://www.youtube.com/embed/8qjpKUmJ9zk', 'STEMex Learning Center Scratch Junior - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Scratch Jr';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/yyRJrItV5ag', '', `id`
+SELECT 'https://www.youtube.com/embed/yyRJrItV5ag', 'Senior Scratch Programming Course - Promotion vidoe - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Scratch Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/yyRJrItV5ag', '', `id`
+SELECT 'https://www.youtube.com/embed/yyRJrItV5ag', 'Senior Scratch Programming Course - Promotion vidoe - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Coding Scratch Intermediate';
 
 
 -- Cyber Virtual Robotics
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', '', `id`
+SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', 'STEMex Cyber Virtual Robotics - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Cyber Virtual Robotics Elementary';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', '', `id`
+SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', 'STEMex Cyber Virtual Robotics - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Cyber Virtual Robotics Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', '', `id`
+SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', 'STEMex Cyber Virtual Robotics - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Cyber Virtual Robotics Intermediate';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', '', `id`
+SELECT 'https://www.youtube.com/embed/HY4eXuKFLts', 'STEMex Cyber Virtual Robotics - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Cyber Virtual Robotics Master';
 
 -- Lego Robotics
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/f8D1D_KE1cc', '', `id`
+SELECT 'https://www.youtube.com/embed/f8D1D_KE1cc', 'STEMex Lego Wedo course - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Lego Robotics Elementary - Wedo';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/YCf19REI2C8', '', `id`
+SELECT 'https://www.youtube.com/embed/YCf19REI2C8', 'STEMex Robotics & Coding Regular Workshop - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Lego Robotics Introductory - EV3';
 
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/YCf19REI2C8', '', `id`
+SELECT 'https://www.youtube.com/embed/YCf19REI2C8', 'STEMex Robotics & Coding Regular Workshop - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Lego Robotics Intermediate - EV3';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/YCf19REI2C8', '', `id`
+SELECT 'https://www.youtube.com/embed/YCf19REI2C8', 'STEMex Robotics & Coding Regular Workshop - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Lego Robotics Advanced - EV3';
 
 
 -- VEX Robotics
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/VEGboBxmKG8', '', `id`
+SELECT 'https://www.youtube.com/embed/VEGboBxmKG8', 'STEMex VEX Robotics - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'VEX Robotics Introductory';
 
@@ -130,84 +130,84 @@ WHERE `description` = 'VEX Robotics Introductory';
 -- AppInventor Mobile Apps Development
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', '', `id`
+SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', 'STEMex AppInventor Mobile Apps Development + A.I. - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'AppInventor Mobile Apps Development Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', '', `id`
+SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', 'STEMex AppInventor Mobile Apps Development + A.I. - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'AppInventor Mobile Apps Development Intermediate';
 
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', '', `id`
+SELECT 'https://www.youtube.com/embed/zbpzr_hYwtg', 'STEMex AppInventor Mobile Apps Development + A.I. - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'AppInventor Mobile Apps Development Advanced';
 
 
 -- A.I. & Machine Learning
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', '', `id`
+SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', 'STEMex A.I. Coding and Machine Learning - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'A.I. & Machine Learning Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', '', `id`
+SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', 'STEMex A.I. Coding and Machine Learning - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'A.I. & Machine Learning Intermediate';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', '', `id`
+SELECT 'https://www.youtube.com/embed/n-IOSJCYJyM', 'STEMex A.I. Coding and Machine Learning - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'A.I. & Machine Learning Advanced';
 
 -- 3D Design & Printing
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/2JnQIQFUaUw', '', `id`
+SELECT 'https://www.youtube.com/embed/2JnQIQFUaUw', 'STEMex 3D Design & Printing - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = '3D Design & Printing Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/2JnQIQFUaUw', '', `id`
+SELECT 'https://www.youtube.com/embed/2JnQIQFUaUw', 'STEMex 3D Design & Printing - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = '3D Design & Printing Intermediate';
 
 -- Micro:bit Digital Making
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://youtube.com/embed/OlIW3TG-Yuw?feature=share', '', `id`
+SELECT 'https://youtube.com/embed/OlIW3TG-Yuw', 'STEMex Microbit Digital Making & Coding Course - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Micro:bit Digital Making Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/97z-_o6PRZE', '', `id`
+SELECT 'https://www.youtube.com/embed/97z-_o6PRZE', 'STEMex Micro:bit Digital Making - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Micro:bit Digital Making Intermediate';
 
 
 -- AR/VR
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/3Umu5vidiGw', '', `id`
+SELECT 'https://www.youtube.com/embed/3Umu5vidiGw', 'STEMex Academy - Future STEM - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'AR/VR Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/3Umu5vidiGw', '', `id`
+SELECT 'https://www.youtube.com/embed/3Umu5vidiGw', 'STEMex Academy - Future STEM - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'AR/VR Intermediate';
 
 -- Smart City
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/oHDqxFhU96w', '', `id`
+SELECT 'https://www.youtube.com/embed/oHDqxFhU96w', 'STEMex Smart City & Energy 智慧城市及能源 - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Smart City Elementary';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/x2SZm5OxpQs', '', `id`
+SELECT 'https://www.youtube.com/embed/x2SZm5OxpQs', 'STEMex Summer Camp - HK2047 - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Smart City Introductory';
 
 INSERT INTO curriculum_course_youtube_video_entries (`url`, `title`, `entry_id`)
-SELECT 'https://www.youtube.com/embed/oHDqxFhU96w', '', `id`
+SELECT 'https://www.youtube.com/embed/oHDqxFhU96w', 'STEMex Smart City & Energy 智慧城市及能源 - YouTube', `id`
 FROM curriculum_entries
 WHERE `description` = 'Smart City Intermediate';
