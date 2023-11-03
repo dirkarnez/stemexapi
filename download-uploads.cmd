21b875e0e5b97ec392c06176e3420a182c514449
dirkarnez <smalldirkalex@gmail.com>
2023-11-03 10:25:49 +0800

    - update download-uploads.cmd

--- download-uploads.cmd
+++ download-uploads.cmd
@@ -1,6 +1,6 @@
 @echo off
 set PATH=%USERPROFILE%\Downloads\putty;C:\Program Files (x86)\PuTTY;
 
-echo y | pscp -i %USERPROFILE%\Downloads\alex.ppk -r ubuntu@ec2-43-198-151-195.ap-east-1.compute.amazonaws.com:/home/ubuntu/uploads/ ./uploads/
+echo y | pscp -i %USERPROFILE%\Downloads\stemexkeys\alex.ppk -r ubuntu@ec2-43-198-151-195.ap-east-1.compute.amazonaws.com:/home/ubuntu/uploads/ ./uploads/
 
 pause
