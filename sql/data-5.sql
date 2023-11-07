0f45b416f0747e2eaa90cae1a57e42892a0abf1d
dirkarnez <smalldirkalex@gmail.com>
2023-11-07 13:28:09 +0800

    - add sql/data-5.sql

---
+++ sql/data-5.sql
@@ -0,0 +1,22 @@
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-09-29 20:54:29.381','2023-09-29 20:54:29.381', users.`id` FROM users WHERE users.`user_name` = 'leungloretta';   
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-14 03:21:04.615','2023-08-14 03:21:04.615', users.`id` FROM users WHERE users.`user_name` = 'jovy';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-22 04:43:24.594','2023-08-22 04:43:24.594', users.`id` FROM users WHERE users.`user_name` = 'joe';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-29 19:09:20.662','2023-10-29 19:09:20.662', users.`id` FROM users WHERE users.`user_name` = 'jovy';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-07 09:21:38.596','2023-10-07 09:21:38.596', users.`id` FROM users WHERE users.`user_name` = 'joe';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-17 21:57:32.650','2023-08-17 21:57:32.650', users.`id` FROM users WHERE users.`user_name` = 'joe';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-09-22 00:17:58.140','2023-09-22 00:17:58.140', users.`id` FROM users WHERE users.`user_name` = 'leungloretta';  
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-09 23:07:11.720','2023-08-09 23:07:11.720', users.`id` FROM users WHERE users.`user_name` = 'joe';
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-30 11:08:55.721','2023-08-30 11:08:55.721', users.`id` FROM users WHERE users.`user_name` = 'leungloretta';   
+INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-03 13:21:56.625','2023-10-03 13:21:56.625', users.`id` FROM users WHERE users.`user_name` = 'prospect123';
+
+
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-10 07:27:36.233','2023-10-10 07:27:36.233', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-09-01 19:27:34.319','2023-09-01 19:27:34.319', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-09-14 12:56:20.764','2023-09-14 12:56:20.764', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-12 07:19:48.689','2023-10-12 07:19:48.689', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-09-01 15:42:54.097','2023-09-01 15:42:54.097', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-05 23:49:56.163','2023-08-05 23:49:56.163', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-11 03:52:51.313','2023-10-11 03:52:51.313', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-30 15:00:34.081','2023-08-30 15:00:34.081', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-08-22 16:09:00.177','2023-08-22 16:09:00.177', users.`id` FROM users WHERE users.`user_name` = 'prospect123';    
+  INSERT INTO testing.user_activities  (`created_at`,`updated_at`,`user_id`) SELECT '2023-10-22 01:35:05.746','2023-10-22 01:35:05.746', users.`id` FROM users WHERE users.`user_name` = 'prospect123';   
