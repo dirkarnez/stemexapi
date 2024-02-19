d37178910d0aec3bfb5861186f2efa9bb9147ad3
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 13:13:18 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -621,7 +621,8 @@ func CreateOrUpdateCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.D
 							entityPresentationNote.ID = presentationNoteIDUUID
 						}
 
-						_, presentationNoteFileHeader, err := ctx.Request().FormFile(fmt.Sprintf("levels.%d.lessons.%d.presentation_notes.%d.presentation_note_file", i, j, k))
+						entityPresentationNote.ResourseID = presentationNote
+						_, presentationNoteFileHeader, err := ctx.Request().FormFile(fmt.Sprintf("levels.%d.lessons.%d.presentation_notes.%d.file", i, j, k))
 						if err == nil {
 							file, err := utils.SaveUploadV2(presentationNoteFileHeader, &entityPresentationNote.ResourseID, []string{utils.PrefixCourseResourses, curriculumEntry.Description}, s3, tx, ctx)
 							if err != nil {
