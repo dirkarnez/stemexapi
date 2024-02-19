7b4139c219b5b5f0a2df29818f6ba197c977bce1
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 09:57:57 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -431,12 +431,13 @@ func CreateOrUpdateCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.D
 		err = q.Transaction(func(tx *query.Query) error {
 			var curriculumEntry model.CurriculumEntry = model.CurriculumEntry{}
 
-			var IDUUID model.UUIDEx
+			var IDUUIDPtr *model.UUIDEx
 			if len(form.ID) > 1 {
 				IDUUID, err = model.ValidUUIDExFromIDString(form.ID)
 				if err != nil {
 					return err
 				}
+				IDUUIDPtr
 			}
 
 			curriculumEntry.ID = model.ValidUUIDExFromIDString()
