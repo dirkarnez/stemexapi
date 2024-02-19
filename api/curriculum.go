b0425b4d14b7d8520d903b05004e1453027e849f
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 20:28:44 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -533,7 +533,9 @@ func GetCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.DB) context.
 
 					teachers, err = tx.CurriculumCourseLevelLessonResources.
 						Select(q.CurriculumCourseLevelLessonResources.ALL).
+						LeftJoin(q.CurriculumCourseLessonResourceType, q.CurriculumCourseLevelLessonResources.ResourseTypeID.EqCol(q.CurriculumCourseLessonResourceType.ID)).
 						Where(q.CurriculumCourseLevelLessonResources.LessonID.Eq(curriculumCourseLevelLesson.ID)).
+						Where(q.CurriculumCourseLessonResourceType.Name.Eq("presentation_notes")).
 						Preload(field.Associations).
 						Find()
 					if err != nil {
@@ -549,7 +551,9 @@ func GetCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.DB) context.
 
 					miscs, err = tx.CurriculumCourseLevelLessonResources.
 						Select(q.CurriculumCourseLevelLessonResources.ALL).
+						LeftJoin(q.CurriculumCourseLessonResourceType, q.CurriculumCourseLevelLessonResources.ResourseTypeID.EqCol(q.CurriculumCourseLessonResourceType.ID)).
 						Where(q.CurriculumCourseLevelLessonResources.LessonID.Eq(curriculumCourseLevelLesson.ID)).
+						Where(q.CurriculumCourseLessonResourceType.Name.Eq("presentation_notes")).
 						Preload(field.Associations).
 						Find()
 					if err != nil {
