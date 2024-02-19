a57a178f337996dfae712676c2b0eee7a57ab0ae
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 13:31:21 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -369,7 +369,10 @@ func CreateOrUpdateCurriculumCourseType(s3 *utils.StemexS3Client, dbInstance *go
 			returnForm.Description = entryToSave.Description
 			returnForm.ID = entryToSave.ID.ToString()
 			returnForm.IconID = entryToSave.IconID.ToString()
-			returnForm.ParentID = entryToSave.ParentID.ToString()
+
+			if entryToSave.ParentID != nil {
+				returnForm.ParentID = (*entryToSave.ParentID).ToString()
+			}
 
 			ctx.JSON(returnForm)
 		}
