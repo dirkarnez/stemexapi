7d8a1caba23fb7c737f0c3e43e8484b7e3e94e89
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 09:53:56 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -427,13 +427,13 @@ func CreateOrUpdateCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.D
 			}
 
 			var q = query.Use(dbInstance)
-			var curriculumEntry model.CurriculumEntry{}
-			q.Transaction(func(tx *query.Query) error {
+
+			err = q.Transaction(func(tx *query.Query) error {
 				var err error
 				user, err = tx.CurriculumEntry.Clauses(clause.OnConflict{
 					UpdateAll: true,
 				}).
-				Create(&curriculumEntry)
+					Create(&curriculumEntry)
 				if err != nil {
 					return err
 				}
