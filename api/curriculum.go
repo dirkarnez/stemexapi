27ed71b649dd508bdc59d3aaa0e0c96773139159
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 19:35:46 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -460,7 +460,7 @@ func GetCurriculumCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.DB
 			}
 
 			for _, blog := range curriculumCourseBlogEntries {
-				returnForm.BlogEntries = append(returnForm.BlogEntries, dto.CurriculumCourseYoutubeVideoEntries{
+				returnForm.BlogEntries = append(returnForm.BlogEntries, dto.CurriculumCourseBlogEntries{
 					URL: youtube.URL,
 				})
 			}
