7414a804562c1875c5254e51fec7aa8e25b4ce6d
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 09:59:43 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -444,7 +444,7 @@ func CreateOrUpdateCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.D
 				if err != nil {
 					return err
 				}
-				curriculumEntry.ID = IconIDUUID
+				curriculumEntry.IconID = IconIDUUID
 			}
 
 			curriculumEntry.IconID = 
