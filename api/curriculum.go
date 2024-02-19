e94371862caa700ccccac54f9bc6baa500a93d6e
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 19:42:29 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -461,7 +461,7 @@ func GetCurriculumCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.DB
 				returnForm.ParentID = (*(*curriculumEntry).ParentID).ToString()
 			}
 
-			returnForm.CourseID = string(curriculumCourse.ID)
+			returnForm.CourseID = (*curriculumCourse).ID
 			returnForm.CurriculumPlanID
 			returnForm.CurriculumPlanFileName
 
