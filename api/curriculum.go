28e3aa60012ad46d9bc37e9e0b1bb2488593c6e9
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 16:21:37 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -402,16 +402,16 @@ func CreateOrUpdateCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.D
 		// course_levels[*].Lessons[*].MiscMaterials(id, file_name, file)
 		// course id
 		type Form struct {
-			ID                     string                                    `form:"id"`
-			IconID                 string                                    `form:"icon_id"`
-			Description            string                                    `form:"description"`
-			ParentID               string                                    `form:"parent_id"`
-			CourseID               string                                    `form:"course_id"`
-			CurriculumPlanID       string                                    `form:"curriculum_plan_id"`
-			CurriculumPlanFileName string                                    `form:"curriculum_plan_file_name"` // uploaded
-			BlogEntries            []dto.CurriculumCourseBlogEntries         `form:"blog_entries"`
-			YoutubeVideoEntries    []dto.CurriculumCourseYoutubeVideoEntries `form:"youtube_video_entries"`
-			Levels                 []dto.CurriculumCourseLevels              `form:"levels"`
+			ID                     string                                    `form:"id" json:"id"`
+			IconID                 string                                    `form:"icon_id" json:"id`
+			Description            string                                    `form:"description" json:"id`
+			ParentID               string                                    `form:"parent_id" json:"id`
+			CourseID               string                                    `form:"course_id" json:"id`
+			CurriculumPlanID       string                                    `form:"curriculum_plan_id" json:"id`
+			CurriculumPlanFileName string                                    `form:"curriculum_plan_file_name" json:"id` // uploaded
+			BlogEntries            []dto.CurriculumCourseBlogEntries         `form:"blog_entries" json:"id`
+			YoutubeVideoEntries    []dto.CurriculumCourseYoutubeVideoEntries `form:"youtube_video_entries" json:"id`
+			Levels                 []dto.CurriculumCourseLevels              `form:"levels" json:"id`
 		}
 
 		var returnForm Form
