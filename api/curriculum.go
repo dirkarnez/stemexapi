6aec4bbe27333bf9f8211f64d54d534b485a278c
dirkarnez <smalldirkalex@gmail.com>
2024-02-19 20:21:07 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -498,6 +498,8 @@ func GetCurriculumCourse(s3 *utils.StemexS3Client, dbInstance *gorm.DB) context.
 					if err != nil {
 						return err
 					}
+					for _, curriculumCourseLevelLesson := range ppts {
+					}
 
 					pptDTO := dto.CurriculumCourseLevelLessonResources{
 						ID: ppt.ID.
