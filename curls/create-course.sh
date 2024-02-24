f1740641569459e306c554afa8bd63bfa3a441ce
dirkarnez <smalldirkalex@gmail.com>
2024-02-24 17:00:38 +0800

    - wip

---
+++ curls/create-course.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
+# use this to parse a json file to array of objects and foreach(element => console.log(element.name))
+parent_id=$(jq-windows-amd64.exe --raw-output '.id' "./Coding Roblox.json")
+
+
+# @REM @REM INSERT INTO curriculum_entries (`description`, `parent_id`, `icon_id`, `seq_no_same_level`)
+# @REM @REM SELECT '', curriculum_entries.`id`, files.`id`, 0
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft'
+# @REM @REM AND files.`server_physical_file_name` = '';
+
+
+
+# @REM @REM INSERT INTO curriculum_entries (`description`, `parent_id`, `icon_id`, `seq_no_same_level`)
+# @REM @REM SELECT 'Coding Minecraft Introductory', curriculum_entries.`id`, files.`id`, 1
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 2-Introductory-min.png';
+
+# @REM @REM INSERT INTO curriculum_entries (`description`, `parent_id`, `icon_id`, `seq_no_same_level`)
+# @REM @REM SELECT 'Coding Minecraft Intermediate', curriculum_entries.`id`, files.`id`, 2
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 2-Intermediate-min.png';
+
+# @REM @REM INSERT INTO curriculum_entries (`description`, `parent_id`, `icon_id`, `seq_no_same_level`)
+# @REM @REM SELECT 'Coding Minecraft Advanced', curriculum_entries.`id`, files.`id`, 3
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 2-Advanced-min.png';
+
+
+# @REM @REM INSERT INTO curriculum_entries (`description`, `parent_id`, `icon_id`, `seq_no_same_level`)
+# @REM @REM SELECT 'Coding Minecraft Master', curriculum_entries.`id`, files.`id`, 4
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 2-Master-min.png';
+
+
+# @REM @REM INSERT INTO curriculum_course_information_entries (`icon_id`, `title`, `content`, `entry_id`)
+# @REM @REM SELECT files.`id`, 'A - Medieval Machinations Redstone', 'This course will introduce students to use Redstone, electrical circuitry, in a Medieval Theme. Students make mine carts to gather resources, collaborate to build their kingdom and to defend their castle. They are going to experience a lot of creation, adventure and exploration.', curriculum_entries.`id`
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE curriculum_entries.`description` = 'Coding Minecraft Elementary'
+# @REM @REM AND files.`server_physical_file_name` = 'schedule-details/codingMineCraftElementry/elementry1.png';
+
+# @REM @REM INSERT INTO curriculum_course_information_entries (`icon_id`, `title`, `content`, `entry_id`)
+# @REM @REM SELECT files.`id`, 'B - Theme Park', 'Everyone loves amusement theme parks. Students will have to navigate a number of engineering and teamwork challenges. They draft blueprints and plan for their parks build and create it collaboratively. They will play around and make it as much like the process of designing a real amusement park.', curriculum_entries.`id`
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE `description` = 'Coding Minecraft Elementary'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 3-Elementary B.png';
+
+# @REM @REM INSERT INTO curriculum_course_information_entries (`icon_id`, `title`, `content`, `entry_id`)
+# @REM @REM SELECT files.`id`, 'C - Travelling Into the Future', 'Minecraft is a versatile and fantasy game in which players are immersed in a world made up of various kinds of blocks. In order to use blocks, players must gather resources from the world they are in and can use them to craft new materials, tools or potions. In this lesson, students will be introduced to Minecraft in a future world that will teach them the basics of playing Minecraft and will teach them to work as a team to overcome obstacles and build a survival area in a new area.', curriculum_entries.`id`
+# @REM @REM FROM curriculum_entries, files 
+# @REM @REM WHERE `description` = 'Coding Minecraft Elementary'
+# @REM @REM AND files.`server_physical_file_name` = 'upcoming-schedule/codingMinecraft/Level 3-Elementary C.png';
+
+
+
+icon_file="upcoming-schedule/codingMinecraft/Level 2-Elementary-min.png"
+description="Coding Minecraft Elementary"
+
+curl -X POST --location "https://localhost/api/curriculum-course" -b cookie.txt --insecure \
+--form "parent_id=\"$parent_id\"" \
+--form "icon_file=@\"../uploads/$icon_file\"" \
+--form "blog_entries.0.external_url=\"https://hk.stemex.org/java-your-way-through-minecraft/\"" \
+--form "blog_entries.0.title=\"JAVA YOUR WAY THROUGH MINECRAFT!\"" \
+--form "blog_entries.1.external_url=\"https://hk.stemex.org/minecraft-kids/\"" \
+--form "blog_entries.1.title=\"Minecraft 編程 - 怎樣提升孩子\"" \
+--form "description=\"%description%\"" \
+--output "%output%.json"
+
+
+# @REM --form "curriculum_plan_file=@\"../external/%curriculum_plan_file%\"" ^
+# @REM --form "youtube_video_entries.0.url=\"https://www.youtube.com/watch?v=ZIW_6rxYNBg\"" ^
+
+--form "levels.0.name=\"A"" ^
+--form "levels.0.icon_file', File]
+--form "levels.0.description=\"sdf"" \
+--form "levels.0.lessons.0.lesson_number=\""" \
+--form "levels.0.lessons.0.presentation_notes.0.id=\""" \
+--form "levels.0.lessons.0.presentation_notes.0.resource_id=\""" \
+--form "levels.0.lessons.0.presentation_notes.0.file', File]
+--form "levels.0.lessons.0.student_notes.0.id=\""" \
+--form "levels.0.lessons.0.student_notes.0.resource_id=\""" \
+--form "levels.0.lessons.0.student_notes.0.file', File]
+--form "levels.0.lessons.0.teacher_notes.0.id=\""" \
+--form "levels.0.lessons.0.teacher_notes.0.resource_id=\""" \
+--form "levels.0.lessons.0.teacher_notes.0.file', File]
+--form "levels.0.lessons.0.misc_materials.0.id=\""" \
+--form "levels.0.lessons.0.misc_materials.0.resource_id=\""" \
+--form "levels.0.lessons.0.misc_materials.0.file', File]
+
+
+
+# @REM set "description=3D Design ^& Printing"
+# @REM set "file=upcoming-schedule/3D_Design_Printing/Level 1-min.png"
+# @REM set "output=3D_Design_Printing"
+
+
+
+
+
+
+
+
+read -p "hahahah"
--- curls/login.cmd
+++ curls/login.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set PATH=C:\Users\19081126D\Downloads\PortableGit\mingw64\bin
+set PATH=%USERPROFILE%\Downloads\PortableGit\mingw64\bin
 
 curl -k  -c cookie.txt --location "https://localhost:443/api/login"  --header "Content-Type: application/json"  --data "  {\"user_name\": \"joe\",\"password\": \"stemex\"}"
 pause
