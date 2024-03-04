0ded951f2b1c797e682f8f2d4f1a5301199130db
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 19:20:05 +0800

    - upload files

--- curls/create-course-types.sh
+++ curls/create-course-types.sh
@@ -8,11 +8,8 @@ function create_course_types () {
 
    echo "$course_desplay_name $course_icon_path $course_json_output"
    curl -X POST --location "https://localhost/api/curriculum-course-type" -b cookie.txt --insecure \
-      --form "description=$course_desplay_name" --form "icon_file=@$course_icon_path" --output "$course_json_output.json"
-   # set "description="
-# set "file="
-# curl -X POST --location "https://localhost/api/curriculum-course-type" -b cookie.txt --insecure ^
-# --form "description=\"%description%\"" --form "icon_file=@\"../uploads/%file%\"" --output "%description%.json"
+      --form "description=$course_desplay_name" \
+      --form "icon_file=@$course_icon_path" --output "$course_json_output.json"
 }
 
 create_course_types "Coding Minecraft" "$USERPROFILE/Downloads/stemex-curriculum/codingMinecraft/Level 1-min.png"
