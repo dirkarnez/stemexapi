10307a3153603b3eecdf3a1e7098fb65bc9dd5e1
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 19:56:53 +0800

    - upload files

--- curls/create-course.sh
+++ curls/create-course.sh
@@ -18,7 +18,7 @@ function create_course () {
         --form "icon_file=@$icon_file" \
         --form "description=$description" \
         --form "curriculum_plan_file=@$3" \
-        --form "youtube_video_entries.0.url=https://www.youtube.com/watch?v=zbpzr_hYwtg" \
+        --form "youtube_video_entries.0.url=$4" \
         --form "blog_entries.0.external_url=https://hk.stemex.org/self-control-app/" \
         --form "blog_entries.0.title=從小培養孩子的自控能力 3款提升自控能力的電子應用程式" \
         --form "levels.0.name=A" \
