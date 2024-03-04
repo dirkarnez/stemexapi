44c7a20af15a08f11f2ec88b2efad60c6a06b16f
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 20:27:11 +0800

    - upload files

--- curls/create-course.sh
+++ curls/create-course.sh
@@ -5,7 +5,7 @@ function create_course () {
     course_type_json_output=$(echo $1 | sed 's/\//_/g' | sed 's/:/_/g')
     parent_id=$(jq-windows-amd64.exe --raw-output '.id' "./$course_type_json_output.json")
     course_json_output=$(echo $description | sed 's/\//_/g' | sed 's/:/_/g')
-    $USERPROFILE/Downloads/curl/curl.exe
+    # $USERPROFILE/Downloads/curl/curl.exe
     
     /C/Windows/System32/curl.exe -X POST --location "https://localhost/api/curriculum-course" -b cookie.txt --insecure \
         --form "parent_id=$parent_id" \
