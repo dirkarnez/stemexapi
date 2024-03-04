c0b8a65db7cb2c2baaa783a1f7e439054747a175
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 19:17:08 +0800

    - upload files

--- curls/create-course-types.sh
+++ curls/create-course-types.sh
@@ -4,7 +4,7 @@ export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
 function create_course_types () {
    course_desplay_name = $1
    course_icon_path = $2
-   $(echo $1 | sed 's/\//-/g')
+   course_json_output = $(echo $1 | sed 's/\//-/g')
 
    echo "Parameter #1 is $1"
    curl -X POST --location "https://localhost/api/curriculum-course-type" -b cookie.txt --insecure \
