f0a850bfbcffb0192d6119b07920b3ad674d5316
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 19:22:34 +0800

    - upload files

--- curls/create-course-types.sh
+++ curls/create-course-types.sh
@@ -20,10 +20,11 @@ create_course_types "Coding Scratch" "$USERPROFILE/Downloads/stemex-curriculum/C
 create_course_types "Cyber Virtual Robotics" "$USERPROFILE/Downloads/stemex-curriculum/cyberVirtualRobotics/Level 1.png"
 create_course_types "Lego Robotics" "$USERPROFILE/Downloads/stemex-curriculum/LEGO_Robotics/Level 1-min.png"
 create_course_types "VEX Robotics" "$USERPROFILE/Downloads/stemex-curriculum/Vex Robotics/Level 1-min.png"
+
 create_course_types "AppInventor Mobile Apps" "$USERPROFILE/Downloads/stemex-curriculum/AppInventor/Level 1-min.png"
-*create_course_types "A.I.& Machine Learning" "$USERPROFILE/Downloads/stemex-curriculum/A.I.& Machine Learning/Level 1-min.png"
-*create_course_types "3D Design & Printing" "$USERPROFILE/Downloads/stemex-curriculum/3D_Design_Printing/Level 1-min.png"
-*create_course_types "Micro:bits"  "$USERPROFILE/Downloads/stemex-curriculum/Micro_bit/Level 1-min.png"
+create_course_types "A.I.& Machine Learning" "$USERPROFILE/Downloads/stemex-curriculum/A.I.& Machine Learning/Level 1-min.png"
+create_course_types "3D Design & Printing" "$USERPROFILE/Downloads/stemex-curriculum/3D_Design_Printing/Level 1-min.png"
+create_course_types "Micro:bits"  "$USERPROFILE/Downloads/stemex-curriculum/Micro_bit/Level 1-min.png"
 create_course_types "AR/VR" "$USERPROFILE/Downloads/stemex-curriculum/AR_VR/Level 1-min.png"
 create_course_types "Smart City" "$USERPROFILE/Downloads/stemex-curriculum/Smart City/Level 1-min.png"
 
