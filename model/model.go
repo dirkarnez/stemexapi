eeb4e6632fc004c30dfbf3919f25efad41914406
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 20:10:16 +0800

    - upload files

--- model/model.go
+++ model/model.go
@@ -91,7 +91,7 @@ type CurriculumEntry struct {
 	IconID UUIDEx `gorm:"column:icon_id;type:binary(16);not null"`
 	Icon   File   `gorm:"foreignKey:IconID"` //constraint:OnDelete:SET NULL
 
-	Description    string  `gorm:"column:description;type:varchar(255);not null;uniqueIndex:idx_description_same_level"`
+	Description    string  `gorm:"column:description;type:varchar(500);not null;uniqueIndex:idx_description_same_level"`
 	ParentID       *UUIDEx `gorm:"column:parent_id;type:binary(16);uniqueIndex:idx_seq_no_same_level;uniqueIndex:idx_description_same_level"`
 	SeqNoSameLevel uint64  `gorm:"column:seq_no_same_level;not null;default:0;uniqueIndex:idx_seq_no_same_level"`
 }
