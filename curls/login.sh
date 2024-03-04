e40327491b2b97495ea29529c6e8dcf9a7d0241d
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 18:47:44 +0800

    - upload files

--- curls/login.sh
+++ curls/login.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
 
-set PATH=%USERPROFILE%\Downloads\PortableGit\mingw64\bin
-
 curl -k -c cookie.txt --location "https://localhost:443/api/login" --header "Content-Type: application/json" --data "{\"user_name\": \"joe\",\"password\": \"stemex\"}"
 pause
 
