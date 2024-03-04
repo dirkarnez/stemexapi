d04972670f64f010f82043a571dbc572f5e07cb4
dirkarnez <smalldirkalex@gmail.com>
2024-03-04 18:47:55 +0800

    - upload files

--- curls/login.sh
+++ curls/login.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
 
-curl -k -c cookie.txt --location "https://localhost:443/api/login" --header "Content-Type: application/json" --data "{\"user_name\": \"joe\",\"password\": \"stemex\"}"
+curl -k -c cookie.txt --location "https://localhost:443/api/login"  \
+--header "Content-Type: application/json" --data "{\"user_name\": \"joe\",\"password\": \"stemex\"}"
 pause
 
