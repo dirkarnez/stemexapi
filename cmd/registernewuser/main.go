e635a55bc817986f9e8cd9402bcf962b0cc69a6f
dirkarnez <smalldirkalex@gmail.com>
2024-03-09 20:08:45 +0800

    - upload files

--- cmd/registernewuser/main.go
+++ cmd/registernewuser/main.go
@@ -62,6 +62,8 @@ func main() {
 			// invalid key
 			return err
 		}
+		
+		field.NewField(q.CurriculumCourse.TableName(), q.CurriculumCourse.ID.ColumnName().String()).IsNotNull().As("is_course")).
 
 		tx.User.UpdateFrom(tx.ParentUserActivating.Select(tx.ParentUserActivating.ActivationKey).Where(tx.ParentUserActivating.ActivationKey.Eq(activationKey))).
 			Where(tx.User.ID.EqCol(tx.ParentUserActivating.UserID)).Update(tx.User.IsActivated, true)
