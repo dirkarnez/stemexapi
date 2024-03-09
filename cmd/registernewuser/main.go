59f68078093ec99be028f4499c93ea7e7d7eecf7
dirkarnez <smalldirkalex@gmail.com>
2024-03-09 20:10:59 +0800

    - upload files

--- cmd/registernewuser/main.go
+++ cmd/registernewuser/main.go
@@ -64,7 +64,7 @@ func main() {
 		}
 		//field.NewField(q.CurriculumCourse.TableName(), q.CurriculumCourse.ID.ColumnName().String()).IsNotNull().As("is_course")).
 		tx.User.UpdateFrom(tx.ParentUserActivating.Select(tx.ParentUserActivating.ActivationKey).Where(tx.ParentUserActivating.ActivationKey.Eq(activationKey))).
-			Where(tx.User.ID.EqCol(tx.ParentUserActivating.UserID)).Update(tx.User.IsActivated, true)
+			Where(tx.User.ID.EqCol(tx.ParentUserActivating.UserID)).Update(tx.User.IsActivated.SetCol(), true)
 
 
 		// tx.User.UpdateFrom(tx.Select(c.ID, c.Address, c.Phone).Where(c.ID.Gt(100))).
