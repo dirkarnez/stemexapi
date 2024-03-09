4387f2510e5a2c1b00492d39c00be80f9ff2270a
dirkarnez <smalldirkalex@gmail.com>
2024-03-09 20:42:19 +0800

    - upload files

--- model/model.go
+++ model/model.go
@@ -60,8 +60,8 @@ type StudentToUser struct {
 	BaseModel
 	UserID UUIDEx `gorm:"column:user_id;type:binary(16);not null"`
 	User   User   `gorm:"foreignKey:UserID;not null"`
-	Name   string `gorm:"column:name;type:varchar(15);unique;not null" json:"name"` //"Student Name",// "SID",
-	Name   string `gorm:"column:name;type:varchar(15);unique;not null" json:"name"`
+	Name   string `gorm:"column:name;type:varchar(15);unique;not null" json:"name"` //"Student Name",// "",
+	SID    string `gorm:"column:name;type:varchar(15);unique;not null" json:"sid"`
 	//
 
 }
