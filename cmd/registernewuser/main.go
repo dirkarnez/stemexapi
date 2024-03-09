0ec4b36409c60a20171e1f816f604a2c950a272a
dirkarnez <smalldirkalex@gmail.com>
2024-03-09 20:13:00 +0800

    - upload files

--- cmd/registernewuser/main.go
+++ cmd/registernewuser/main.go
@@ -66,7 +66,7 @@ func main() {
 		tx.User.
 			UpdateFrom(tx.ParentUserActivating.Select(tx.ParentUserActivating.ActivationKey).Where(tx.ParentUserActivating.ActivationKey.Eq(activationKey))).
 			Where(tx.User.ID.EqCol(tx.ParentUserActivating.UserID)).
-			Update(tx.User.IsActivated.SetCol(tx.ParentUserActivating.ActivationKey.IsNotNull())
+			Update(tx.User.IsActivated.SetCol(tx.ParentUserActivating.ActivationKey.IsNotNull()))
 
 		// tx.User.UpdateFrom(tx.Select(c.ID, c.Address, c.Phone).Where(c.ID.Gt(100))).
 		// 	Where(ua.CompanyID.EqCol(ca.ID)).
