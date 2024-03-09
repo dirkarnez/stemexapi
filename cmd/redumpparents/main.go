a68a735498190a1eb789e21324ed140b29f5cfff
dirkarnez <smalldirkalex@gmail.com>
2024-03-09 20:46:02 +0800

    - upload files

--- cmd/redumpparents/main.go
+++ cmd/redumpparents/main.go
@@ -1,1 +1,16 @@
 https: //sheets.googleapis.com/v4/spreadsheets/1mRMBmxKuReGqp9MvcTiv-Z-QcxSDsHUHKwnxPORcj2Y/values/Form?key=AIzaSyBAuyTYKGijZn3jkwoMDlw0ZsR8JR5iOno
+{
+	"range": "Form!A1:Z1000",
+	"majorDimension": "ROWS",
+	"values": [
+	  [
+		"Username",
+		"Password",
+		"SID",
+		"Student Name",
+		"Parent Contact Number",
+		"Parent Email",
+		"Green Highlight = Working Accs",
+		"Yellow Highlight = Having Class with us now or in the future",
+		"White Highlight = Not Ready Accs Yet"
+	  ],
