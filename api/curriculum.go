e205572c427893f8af351de4cc573dcb1627289a
dirkarnez <smalldirkalex@gmail.com>
2024-04-02 21:16:20 +0800

    - upload files

--- api/curriculum.go
+++ api/curriculum.go
@@ -1411,8 +1411,7 @@ func MapRequestToCurriculumCourseForm(req *http.Request) (*dto.CurriculumCourseF
 
 		var i = 0
 
-		youtubeVideoEntriesBaseKey = "youtube_video_entries[%d]"
-		var lessonsArrayKey = fmt.Sprintf(`levels[%d]`, i, j)
+		var youtubeVideoEntriesBaseKey = "youtube_video_entries[%d]"
 
 		for {
 			var blogEntriesIDKey = fmt.Sprintf(`blog_entries[%d].id`, i)
