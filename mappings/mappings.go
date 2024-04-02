c0547217ebdeeea7f2264714d5fd065302890e1b
dirkarnez <smalldirkalex@gmail.com>
2024-04-02 23:03:20 +0800

    - upload files

--- mappings/mappings.go
+++ mappings/mappings.go
@@ -3,6 +3,7 @@ package mappings
 import (
 	"github.com/dirkarnez/stemexapi/dto"
 	"github.com/dirkarnez/stemexapi/model"
+	"github.com/dirkarnez/stemexapi/utils"
 )
 
 func MapCurriculumCourseFormToCurriculumEntry(form *dto.CurriculumCourseForm, curriculumEntry *model.CurriculumEntry) error {
@@ -17,6 +18,16 @@ func MapCurriculumCourseFormToCurriculumEntry(form *dto.CurriculumCourseForm, cu
 	curriculumEntry.ParentID, err = model.ValidUUIDExPointerFromIDString(form.ParentID)
 	if err != nil {
 		return err
+	}
+
+	form.IconFile
+	_, iconFileHeader, err := ctx.Request().FormFile("icon_file")
+	if err == nil {
+		file, err := utils.SaveUploadV2(iconFileHeader, &curriculumEntry.IconID, []string{utils.PrefixCourseResourses, curriculumEntry.Description}, s3, tx, ctx)
+		if err != nil {
+			return err
+		}
+		curriculumEntry.IconID = file.ID
 	}
 
 	curriculumEntry.IconID, err = model.ValidUUIDExFromIDString(form.IconID)
