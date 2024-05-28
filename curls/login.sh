2aa8a7ea97121816d1d2f97d9bb59db76ff38181
dirkarnez <smalldirkalex@gmail.com>
2024-05-28 21:02:51 +0800

    - remove curls

--- curls/login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
-
-/C/Windows/System32/curl.exe -k -c cookie.txt --location "https://localhost:4443/api/login" \
-    --header "Content-Type: application/json" \
-    --data "{\"user_name\": \"joe\",\"password\": \"stemex\"}"
-
-read -p "done"
--- curls/tree.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
-
-function get_curriculum_tree () {
-    /C/Windows/System32/curl.exe "https://localhost:4443/api/curriculum-tree"  -b cookie.txt --insecure
-}
-
-get_curriculum_tree
