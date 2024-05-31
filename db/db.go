aa13a2f5444583a364287b9aa0855f802a2ccdda
dirkarnez <smalldirkalex@gmail.com>
2024-05-31 15:57:57 +0800

    - wip

--- db/db.go
+++ db/db.go
@@ -6,6 +6,6 @@ import (
 )
 
 func InitConntection() (db *gorm.DB, err error) {
-	dsn := "webadmin:password@tcp(ec2-43-198-151-195.ap-east-1.compute.amazonaws.com:3306)/testing?charset=utf8mb4&parseTime=True"
+	dsn := "webadmin:password@tcp(18.163.71.246:3306)/testing?charset=utf8mb4&parseTime=True"
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
